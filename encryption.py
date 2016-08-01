cc371d6279d2889d38584d2418ff3431d1831988
daniel bauman <danielbauman2000@daniels-Air.fios-router.home>
2016-08-01 10:25:03 -0400

    added args support, secure key handling, removed scramble feature, added file encryption.

--- encryption.py
+++ encryption.py
@@ -1,4 +1,8 @@
 from random import randint
+import random
+import string
+import sys
+import getpass
 class Scramble_unit:
     first = 0
     second = 0
@@ -53,9 +57,13 @@ def encrypt(mess,key):
         enmess += addChars(list(mess)[i],list(key)[i])
     return enmess
 def addChars(a,b):
+    if a == '\n':
+        return a
+    if a == '\t':
+        return a
     for i in range(0,ord(b)):
         if ord(a) == 126:
-            a = chr(32)
+            a = chr(31)
         a = chr(ord(a)+1)
     return a
 def decrypt(mess,key):
@@ -64,13 +72,15 @@ def decrypt(mess,key):
         demess += subChars(list(mess)[i],list(key)[i])
     return demess
 def subChars(a,b):
+    if a == '\n':
+        return a
+    if a == '\t':
+        return a
     for i in range(0,ord(b)):
-        if ord(a) == 33:
+        if ord(a) == 32:
             a = chr(127)
         a = chr(ord(a)-1)
     return a
-run = 1
-print 'Enter e for encryption, d for decryption, and q to quit'
 def format_scram_patt(scram_patt):
     s = ''
     for i in range(0,len(scram_patt.locations)):
@@ -88,6 +98,94 @@ def deformat_scram_patt(form_scram_patt):
         i = i + 1
         scram_patt.add(Scramble_unit(first,second))
     return scram_patt
+def rankey(size):
+    return ''.join(random.choice(string.letters + string.digits + string.punctuation) for _ in range(size))
+def help():
+    print "Use: python .encryption.py -<opt> -<opt> -<opt> ... <args> <args> <args> ..."
+    print "options:"
+    print "encrypting message with key: -ek <message> <key>"
+    print "encrypting message without key: -e  <message> (key will be in included in output)"
+    print "decrypting: -d <message> <key>"
+    print "encrypt file with key: -ef"
+    print "decrypt file with a key: -df"
+    print "To split a file: -s <file>    (result and key file generated)"
+    print "To join a file: -j <Rfile> <Kfile2>"
+    #print "encrypt/decrypt message with scrambling: -s"
+    print "Example: python .encryption.py -ek <message> <key>"
+    #print "Example N.2: python .encryption.py -d -s <message> <key> <scramble>"
+    print "Help menu: -?"
+def encryptFile(filename, key):
+    fr = open(filename, 'r')
+    lines = fr.readlines()
+    fw = open(filename, 'w')
+    for l in lines:
+        fw.write(encrypt(l,key_to_len(key,len(l))))
+    fw.close()
+def decryptFile(filename, key):
+    fr = open(filename, 'r')
+    lines = fr.readlines()
+    fw = open(filename, 'w')
+    for l in lines:
+        fw.write(decrypt(l,key_to_len(key,len(l))))
+    fw.close()
+args = sys.argv
+if len(args) == 1:
+    help()
+elif args[1] == '-?':
+    help()
+elif args[1] == '-ek':
+    if len(args) == 3:
+        key = getpass.getpass()
+        print encrypt(args[2], key_to_len(key, len(args[2])))
+    else:
+        m = raw_input('Message: ')
+        key = getpass.getpass()
+        print encrypt(m, key_to_len(key, len(m)))
+elif args[1] == '-e':
+    if len(args) == 3:
+        key = rankey(len(args[2]))
+        print encrypt(args[2], key)
+        fw = open('key.txt', 'w')
+        fw.write(key)
+        fw.close()
+        print 'Key written in key.txt'
+    else:
+        m = raw_input('Message: ')
+        key = rankey(len(m))
+        print encrypt(args[2], key)
+        fw = open('key.txt', 'w')
+        fw.write(key)
+        fw.close()
+        print 'Key written in key.txt'
+elif args[1] == '-d':
+    if len(args) == 3:
+        key = getpass.getpass()
+        print decrypt(args[2], key_to_len(key, len(args[2])))
+    else:
+        m = raw_input('Message: ')
+        k = getpass.getpass()
+        print decrypt(m, key_to_len(k, len(m)))
+elif args[1] == '-ef':
+    if len(args) == 3:
+        key = getpass.getpass()
+        encryptFile(args[2], key)
+    else:
+        f = raw_input('Filename: ')
+        k = getpass.getpass()
+        encryptFile(f,k)
+elif args[1] == '-df':
+    if len(args) == 3:
+        key = getpass.getpass()
+        decryptFile(args[2], key)
+    else:
+        f = raw_input('Filename: ')
+        k = getpass.getpass()
+        decryptFile(f, k)
+
+else:
+    help()
+
+"""
 while run:
     command = raw_input('Command>> ')
     if command == 'q':
@@ -114,3 +212,4 @@ while run:
         print descram
     else:
         print 'invalid command'
+"""
